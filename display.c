bb5bfac623794df550d554e5537721c9702ae29f
Jacob Lin <lovetaeyeon507@gmail.com>
2025-02-12 01:37:04 +0800

    Add camera moving

--- display.c
+++ display.c
@@ -72,6 +72,48 @@ static void fenster_rect(struct fenster *f, int x, int y, int w, int h,
 #include "types.h"
 #include "util.h"
 
+static uint16_t font5x3[] = {
+    0x0000, 0x2092, 0x002d, 0x5f7d, 0x279e, 0x52a5, 0x7ad6, 0x0012, 0x4494,
+    0x1491, 0x017a, 0x05d0, 0x1400, 0x01c0, 0x0400, 0x12a4, 0x2b6a, 0x749a,
+    0x752a, 0x38a3, 0x4f4a, 0x38cf, 0x3bce, 0x12a7, 0x3aae, 0x49ae, 0x0410,
+    0x1410, 0x4454, 0x0e38, 0x1511, 0x10e3, 0x73ee, 0x5f7a, 0x3beb, 0x624e,
+    0x3b6b, 0x73cf, 0x13cf, 0x6b4e, 0x5bed, 0x7497, 0x2b27, 0x5add, 0x7249,
+    0x5b7d, 0x5b6b, 0x3b6e, 0x12eb, 0x4f6b, 0x5aeb, 0x388e, 0x2497, 0x6b6d,
+    0x256d, 0x5f6d, 0x5aad, 0x24ad, 0x72a7, 0x6496, 0x4889, 0x3493, 0x002a,
+    0xf000, 0x0011, 0x6b98, 0x3b79, 0x7270, 0x7b74, 0x6750, 0x95d6, 0xb9ee,
+    0x5b59, 0x6410, 0xb482, 0x56e8, 0x6492, 0x5be8, 0x5b58, 0x3b70, 0x976a,
+    0xcd6a, 0x1370, 0x38f0, 0x64ba, 0x3b68, 0x2568, 0x5f68, 0x54a8, 0xb9ad,
+    0x73b8, 0x64d6, 0x2492, 0x3593, 0x03e0};
+// clang-format on
+static void fenster_text(struct fenster *f, int x, int y, char *s, int scale,
+                         uint32_t c) {
+  while (*s) {
+    char chr = *s++;
+    if (chr > 32) {
+      uint16_t bmp = font5x3[chr - 32];
+      for (int dy = 0; dy < 5; dy++) {
+        for (int dx = 0; dx < 3; dx++) {
+          if (bmp >> (dy * 3 + dx) & 1) {
+            fenster_rect(f, x + dx * scale, y + dy * scale, scale, scale, c);
+          }
+        }
+      }
+    }
+    x = x + 4 * scale;
+  }
+}
+
+void render_key(struct fenster *f) {
+  char c[32] = {0};
+  char *p = c;
+  for (int i = 0; i < 128; i++) {
+    if (f->keys[i]) {
+      *p++ = i;
+    }
+  }
+  fenster_text(f, 8, 8, c, 16, 0xffffff);
+}
+
 void anime() {
   display_init();
   fenster_open(&f);
@@ -84,20 +126,41 @@ void anime() {
   instance_t inst[1] = {
       {m, {1.0, {0, 60, 0}, {1.0, 0.0, 5}}, tx, dim},
   };
-  int count = 0, not_turned = 1;
+  transform_t cam = {0};
   while (fenster_loop(&f) == 0) {
-    scene_t *s =
-        construct_scene(inst, 1, (transform_t){0, {0, 0, 0}, {0, 0, 0}}, l, 2);
+
+    /* Handle input */
+    if (f.keys['W'])
+      cam.tr.z += 0.1;
+    if (f.keys['S'])
+      cam.tr.z -= 0.1;
+    if (f.keys['A'])
+      cam.tr.x -= 0.1;
+    if (f.keys['D'])
+      cam.tr.x += 0.1;
+
+    /* Esc */
+    if (f.keys[27])
+      break;
+
+    /* Scene rendering */
+    scene_t *s = construct_scene(inst, 1, cam, l, 2);
     s->pl = generate_fov90_planes();
     display_clear();
     render(s);
+
     int64_t time = fenster_time();
     // 60 fps
     if (time - now < 1000 / 60) {
       fenster_sleep(time - now);
     }
     now = time;
+
+    /* Model Rotate */
     inst[0].tr.r.y += 0.5;
+
+    /* Render key pressed */
+    render_key(&f);
   }
   fenster_close(&f);
   free_model(m);
